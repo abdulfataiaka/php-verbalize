02d0408378c684006b0b75dfb16c991ae4fe0a2c
abdulfataiaka <abdulfataiaka@gmail.com>
2020-06-28 17:49:51 +0100

    update readme

--- src/FailureException.php
+++ src/FailureException.php
@@ -4,9 +4,19 @@ namespace Leickon\Action;
 
 use Exception;
 
-class Failure extends Exception {
+class FailureException extends Exception {
+  /**
+   * Value to hold error message
+   * 
+   * @property array[[boolean,any]]
+   */
   public $value;
 
+  /**
+   * Bind value to error object
+   * 
+   * @method
+   */
   public function __construct($value) {
     parent::__construct('Should not be called outside : .define');
     $this->value = $value;
---
+++ src/RequiredException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Leickon\Action;
+
+use Exception;
+
+class RequiredException extends Exception {
+  //=! Perform default exception behaviour
+}
